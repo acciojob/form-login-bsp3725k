fd1b8b42bfbc4c8ca34ea163520bde7209575beb
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-11-28 18:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
-    //Write your code here
-
-}
+function getFormvalue(event) {
+            event.preventDefault(); // Prevent the default form submission
+            // Get the values of the first name and last name fields
+            let fname = document.getElementsByName("fname")[0].value;
+            let lname = document.getElementsByName("lname")[0].value;
+            // Display the full name in an alert
+            alert(`${fname} ${lname}`);
+        }
